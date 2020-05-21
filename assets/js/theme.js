6aaee3298c1a14bcbb1d9d05520af3640c5c89d5
Yasir Ali <sdr.yasir@gmail.com>
2020-05-21 05:09:16 +0500

    test commit

---
+++ assets/js/theme.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.projects-owl-slider').owlCarousel({
+        loop: true,
+        margin: 0,
+        nav: true,
+        items: 5,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        center: true,
+    })
+});
